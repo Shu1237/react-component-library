844e2b14f4eef3141b2fd054124cb4786b305610
Shu1237 <ttei8191@gmail.com>
2025-07-26 14:04:50 +0700

    Remove Textarea component and its related files

--- src/components/Textarea/type.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  value?: string;
-  onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
-  placeholder?: string;
-}

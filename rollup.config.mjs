471e601e6530bdd711360a31abd7aa277866c87f
Shu1237 <ttei8191@gmail.com>
2025-07-28 09:57:40 +0700

    Add @rollup/plugin-terser for minification and update TypeScript declaration settings

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,13 +3,15 @@ import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import terser from '@rollup/plugin-terser';
 import { readFileSync } from "fs";
 
 const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default [
+  // Main build configuration
   {
-    input: "src/index.ts",
+    input: "src/index.ts", 
     output: [
       {
         file: packageJson.main,
@@ -25,22 +27,33 @@ export default [
     plugins: [
       resolve({
         extensions: [".js", ".jsx", ".ts", ".tsx"],
-        skip: ["react", "react-dom"],
-      }),
-      commonjs(),
+        skip: ["react", "react-dom"], // skip these dependencies to avoid bundling them
+      }), // allow importing from node_modules
+      commonjs(), // convert CommonJS to ES6
+      terser({
+        format: {
+          comments: false, // remove comments
+        },
+        compress: {
+          drop_console: true,  // remove console logs
+        },
+      }), // minify the output
       typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
-        declaration: false,
-      }),
-      postcss({ extensions: [".css"], inject: true, extract: false }),
+        tsconfig: "./tsconfig.json", 
+        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"], // exclude test files
+        declaration: false, // disable declaration files generation in this step
+      }),// convert TypeScript to JavaScript
+      postcss({ extensions: [".css"], inject: true, extract: false }), // handle CSS imports
     ],
-    external: ["react", "react-dom", "react/jsx-runtime"],
+    external: ["react", "react-dom", "react/jsx-runtime"],  // ignore these dependencies in the bundle (reduce bundle size)
   },
+  // build file index.d.ts
+  // autocomplete for TypeScript users
+  // cue this library
   {
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
+    plugins: [dts()], // remove .d.ts files from the bundle and remove imports not essential for the library 
+    external: [/\.css$/], // ignore CSS files in the type definitions
   },
 ];
